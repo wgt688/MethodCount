92fa02636a7c32b3547e9bbb08bc828dbc381a2f
王从杰 <756041848@qq.com>
2021-06-08 10:46:33 +0800

    base

---
+++ Object.js
@@ -0,0 +1,104 @@
+/* =========================== Object =========================== */
+class Object() {
+    constructor() {
+        this.length = 0
+        this.name = "Object"
+    }
+    hasOwnProperty() {
+
+    }
+    isPrototypeOf() {
+
+    }
+    propertyIsEnumerable() {
+
+    }
+    toLocaleString() {
+
+    }
+    toString() {
+
+    }
+    valueOf() {
+
+    }
+    __defineGetter__() {
+
+    }
+    __defineSetter__() {
+
+    }
+    __lookupGetter__() {
+
+    }
+    __lookupSetter__() {
+
+    }
+
+    static assign() {
+
+    }
+    static create() {
+
+    }
+    static defineProperties() {
+
+    }
+    static defineProperty() {
+
+    }
+    static entries() {
+
+    }
+    static freeze() {
+
+    }
+    static fromEntries() {
+
+    }
+    static getOwnPropertyDescriptor() {
+
+    }
+    static getOwnPropertyDescriptors() {
+
+    }
+    static getOwnPropertyNames() {
+
+    }
+    static getOwnPropertySymbols() {
+
+    }
+    static getPrototypeOf() {
+
+    }
+    static is() {
+
+    }
+    static isExtensible() {
+
+    }
+    static isFrozen() {
+
+    }
+    static isSealed() {
+
+    }
+    static keys() {
+
+    }
+    static preventExtensions() {
+
+    }
+    static prototype() {
+
+    }
+    static seal() {
+
+    }
+    static setPrototypeOf() {
+
+    }
+    static values() {
+
+    }
+}

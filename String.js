ebb8ecc32da60ceb914d479b008ce72b2c602e50
王从杰 <756041848@qq.com>
2021-06-08 10:50:28 +0800

    base

---
+++ String.js
@@ -0,0 +1,155 @@
+/* =========================== String =========================== */
+class String() {
+    constructor() {
+        this.length = 0
+        this.name = "Array"
+    }
+    static fromCharCode() {
+
+    }
+    static fromCodePoint() {
+
+    }
+
+    anchor() {
+
+    }
+    big() {
+
+    }
+    blink() {
+
+    }
+    bold() {
+
+    }
+    charAt() {
+
+    }
+    charCodeAt() {
+
+    }
+    codePointAt() {
+
+    }
+    concat() {
+
+    }
+    endsWith() {
+
+    }
+    fixed() {
+
+    }
+    fontcolor() {
+
+    }
+    fontsize() {
+
+    }
+    includes() {
+
+    }
+    indexOf() {
+
+    }
+    italics() {
+
+    }
+    lastIndexOf() {
+
+    }
+    link() {
+
+    }
+    localeCompare() {
+
+    }
+    match() {
+
+    }
+    matchAll() {
+
+    }
+    normalize() {
+
+    }
+    padEnd() {
+
+    }
+    padStart() {
+
+    }
+    repeat() {
+
+    }
+    replace() {
+
+    }
+    replaceAll() {
+
+    }
+    search() {
+
+    }
+    slice() {
+
+    }
+    small() {
+
+    }
+    split() {
+
+    }
+    startsWith() {
+
+    }
+    strike() {
+
+    }
+    sub() {
+
+    }
+    substr() {
+
+    }
+    substring() {
+
+    }
+    sup() {
+
+    }
+    toLocaleLowerCase() {
+
+    }
+    toLocaleUpperCase() {
+
+    }
+    toLowerCase() {
+
+    }
+    toString() {
+
+    }
+    toUpperCase() {
+
+    }
+    trim() {
+
+    }
+    trimEnd() {
+
+    }
+    trimLeft() {
+
+    }
+    trimRight() {
+
+    }
+    trimStart() {
+
+    }
+    valueOf() {
+
+    }
+}
